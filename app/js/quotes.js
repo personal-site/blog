7dd3c6e23f5932383d47fc5c65d9b63614b81d0c
Chris Vogt <mail@chrisvogt.me>
2015-10-20 02:14:06 -0700

    (front-end) Refactor quotes to use DocumentFragments.

--- app/js/quotes.js
+++ app/js/quotes.js
@@ -7,14 +7,26 @@
 	    $.ajax({
 	        url: 'https://cdn.rawgit.com/chrisvogt/49b51791348a09cbddb0/raw/8e9cd6bc0b6261f3a1fc5fd65574d5d4e6468ecb/book-quotes.json',
 	        success: function(data) {
-	            var quote = $('#quote .orbit').empty();
+				var $elem = $('#quote .orbit').empty(),
+					frag = document.createDocumentFragment();
 
-	            $.each(data.quotes, function(i) {
-	            	quote.append($('<li>', {
-	            		html: '<div><blockquote>' + data.quotes[i].text + '<cite>' + data.quotes[i].cite + '</cite></blockquote></div>'
-	            	}));
+	            $.each(data.quotes, function(i, quote) {
+	            	var li 	 = document.createElement( 'li' ),
+            			div  = document.createElement( 'div' ),
+            			bq 	 = document.createElement( 'blockquote' ),
+            			cite = document.createElement( 'cite' );
+
+        			bq.innerHTML   = quote.text;
+        			cite.innerHTML = quote.cite ;
+
+        			li.appendChild( div )
+        			  .appendChild( bq )
+        			  .appendChild( cite );
+
+            		frag.appendChild( li );
 	            });
 
+	            $elem.append( frag );
 	            $(document).foundation('orbit', 'reflow');
 	        }
 	    });
