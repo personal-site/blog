4a0867fb1f52c75ebc48059257a7ffa9564be360
Chris Vogt <mail@chrisvogt.me>
2015-10-22 14:58:04 -0700

    (front end) Enhances the projects section: adds a tech bar to the project modal, rearranges the project modal styles, and refactors the projects section JavaScript.

--- app/js/projects.js
+++ app/js/projects.js
@@ -1,71 +1,99 @@
 'use strict';
 
-(function(){
-	var Projects = {};
+(function() {
+  var Projects = {};
 
-	Projects.init = function() {
-	    $.ajax({
-	        url: 'https://projects.chrisvogt.me/api/1.1/all.json',
-	        success: function(data) {
-	            $('#project-list').empty();
-	            var frag = document.createDocumentFragment();
+  Projects.init = function() {
+    $.ajax({
+      url: 'https://projects.chrisvogt.me/api/1.2/all.json',
+      success: function(data) {
+        $('#project-list').empty();
+        var frag = document.createDocumentFragment();
 
-	            $.each(data.projects, function(i, project) {
-	            	// generate elements
-	            	var li 		= document.createElement( 'li' ),
-	            		link 	= document.createElement( 'a' ),
-	            		thumb 	= document.createElement( 'img' );
+        $.each(data.projects, function(i, project) {
+          // generate elements
+          var li = document.createElement('li'),
+            link = document.createElement('a'),
+            thumb = document.createElement('img');
 
-	            	// set attributes
-	            	$(li).attr({
-	            		'project-id': project.id,
-	            		'project-name': project.name,
-	            		'project-github': project.github,
-	            		'project-demo': project.demo,
-	            		'project-banner': project.banner
-	            	});
-	            	$(link).attr('href', '#modal');
-	            	$(thumb).attr({
-	            		'class': 'image-circle',
-	            		'src': project.thumb
-	            	});
+          // set attributes
+          $(li).attr({
+            'project-id': project.id,
+            'project-name': project.name,
+            'project-github': project.github,
+            'project-demo': project.demo,
+            'project-banner': project.banner,
+            'project-tech': project.tech
+          });
+          $(link).attr('href', project.github);
 
-	            	// matryoshka
-	            	li.appendChild( link );
-	            	link.appendChild( thumb );
-	            	frag.appendChild(li);
-	            });
+          $(thumb).attr({
+            'class': 'image-circle',
+            'src': project.thumb
+          });
 
-            	$('#project-list')[0].appendChild( frag );
+          // matryoshka
+          li.appendChild(link);
+          link.appendChild(thumb);
+          frag.appendChild(li);
+        });
 
-				$( 'ul#project-list a' ).click(function(e) {
-					e.preventDefault();
-					var li = e.target.parentElement.parentElement;
-					$('#modal-project h3').text(li.getAttribute('project-name'));
-					$('#modal-project img').attr({src: li.getAttribute('project-banner')});
-					if (li.getAttribute('project-github')) {
-						$('#modal-project .project-source').removeClass('disabled');
-						$('#modal-project .project-source').attr({
-							href: 'https://github.com/' + li.getAttribute('project-github'),
-							title: li.getAttribute('project-name') + ' on GitHub'
-						});
-					} else {
-						$('#modal-project .project-source').addClass('disabled');
-					}
-					if (li.getAttribute('project-demo')) {
-						$('#modal-project .project-demo').removeClass('disabled');
-						$('#modal-project .project-demo').attr({
-							href: li.getAttribute('project-demo'),
-							title: li.getAttribute('project-name') + ' live demo'
-						});
-					} else {
-						$('#modal-project .project-demo').addClass('disabled');
-					}
-					$('[data-remodal-id=modal]').remodal({hashTracking: false}).open();
-				});
-	        }
-	    });
-	};
+        $('#project-list')[0].appendChild(frag);
 
-	Projects.init();
+        $('ul#project-list a').click(function(e) {
+          e.preventDefault();
+          var project = e.target.parentElement.parentElement;
+
+          $('#modal-project h3').text(project.getAttribute('project-name'));
+          $('#modal-project img').attr({src: project.getAttribute('project-banner')});
+
+          // build the tech bar
+          if (project.getAttribute('project-tech')) {
+            var techs = project.getAttribute('project-tech'),
+                frag  = document.createDocumentFragment();
+
+            $.each(techs.split(','), function(i, tech) {
+              var li = document.createElement('li'),
+                  t  = document.createTextNode(tech);
+
+              li.className = tech;
+              li.appendChild(t);
+              frag.appendChild(li);
+            });
+
+            $('ul#tech').empty().append(frag);
+          }
+
+          // button: view source
+          if (project.getAttribute('project-github')) {
+            $('#modal-project .project-source').removeClass('disabled');
+            $('#modal-project .project-source').attr({
+              href: 'https://github.com/' + project.getAttribute('project-github'),
+              title: project.getAttribute('project-name') + ' on GitHub'
+            });
+          } else {
+            $('#modal-project .project-source').addClass('disabled');
+          }
+
+          // button: demo
+          if (project.getAttribute('project-demo')) {
+            $('#modal-project .project-demo').removeClass('disabled');
+            $('#modal-project .project-demo').attr({
+              href:  project.getAttribute('project-demo'),
+              title: project.getAttribute('project-name') + ' live demo'
+            });
+          } else {
+            $('#modal-project .project-demo').addClass('disabled');
+          }
+
+          // launch the modal
+          $('[data-remodal-id=modal]').remodal({
+            hashTracking: false
+          }).open();
+        });
+      }
+    });
+  };
+
+  Projects.init();
 })();
