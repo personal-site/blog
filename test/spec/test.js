fc1d96980bb841ee7e1a747b1aecb7c385f9961b
Chris Vogt <mail@chrisvogt.me>
2017-01-02 21:32:43 -0700

    Adds initial JS tests.

--- test/spec/test.js
+++ test/spec/test.js
@@ -1,10 +1,29 @@
-(function () {
+(function() {
   'use strict';
 
-  describe('Top-level object', function () {
-    describe('C1V0 object', function () {
-      it('should be an object', function () {
-        expect(C1V0).to.be.an('object');
+  describe('C1V0', function() {
+    describe('Common submodule', function() {
+      var common = C1V0.common;
+
+      it('should be loaded and available', function() {
+        assert.typeOf(common, 'object'); // without optional message
+      });
+
+      describe('capitalize()', function() {
+        var phrase = 'test',
+          result = common.capitalize(phrase);
+
+        it('should return a string', function() {
+          assert.typeOf(result, 'string'); // without optional message
+        });
+
+        it('should return a string whose length matches the input', function() {
+          assert.lengthOf(result, phrase.length, 'output length matches input length')
+        });
+
+        it('should capitalize the input\'s first character', function() {
+          assert.equal(phrase[0].toUpperCase(), result[0]);
+        });
       });
     });
   });
