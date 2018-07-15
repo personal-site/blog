f63be5ec89f4ea9d3942abe0bea2223830902c55
Chris Vogt <mail@chrisvogt.me>
2018-07-15 16:49:38 -0700

    fix(ig-token): update access token (#63)

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -78,7 +78,7 @@ const C1V0 = C1V0 || {
       get: 'user',
       userId: '1546421127',
       clientId: '7e07fcf783e746b3a236341049fa3cc0',
-      accessToken: '1546421127.1677ed0.6d9d503e95e649aab4232a94e0e1dd6f',
+      accessToken: '1546421127.1677ed0.362078a0d37a4fcb8bcbf520a4459cdd',
       sortBy: 'most-recent',
       limit: 18,
       template: '<li><a href="{{link}}" class="hvr-shadow-radial" title="View on Instagram"><img src="{{image}}" alt="{{caption}}"></a></li>',
