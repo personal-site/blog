59c0d8cb59e3318bec33c7a726c477b2cb6a9da8
Chris Vogt <mail@chrisvogt.me>
2018-11-04 23:40:44 -0800

    refactor: social profiles to ES6 without jQuery (#67)

---
+++ app/scripts/social-profiles.js
@@ -0,0 +1,43 @@
+(async () => {
+  const dom = {
+    select: document.querySelector.bind(document)
+  };
+
+  const template = dom.select('#social-item-template').content;
+  const container = dom.select('#social-profiles');
+
+  try {
+    const profiles = await $.getJSON({ url: 'https://chrisvogt.firebaseio.com/v1/profiles.json' });
+    const fragment = document.createDocumentFragment();
+
+    if (!profiles.length) {
+      throw new Error('No profiles found.');
+    }
+
+    for (const profile of profiles) {
+      const content = template.cloneNode(true);
+
+      const {
+        href,
+        icon,
+        name
+      } = profile;
+
+      const iconCss = icon.split(' ');
+      content.querySelector('.social-item-icon').classList.add(...iconCss);
+
+      const link = content.querySelector('.social-item-link');
+      link.title = `Chris Vogt on ${ name }`;
+      link.href = href;
+
+      fragment.appendChild(document.importNode(content, true));
+    }
+
+    container.innerHTML = '';
+    container.appendChild(document.importNode(fragment, true));
+
+  } catch (error) {
+    console.warn('Error loading social profiles section', error);
+    dom.select('#social').classList.add('hidden');
+  }
+})();
