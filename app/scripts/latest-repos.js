6e8fece392cd1b5f0181787d42a784fb028199a1
Chris Vogt <mail@chrisvogt.me>
2018-11-19 23:42:23 -0800

    feat: add a placeholder animation for latest repos items

--- app/scripts/latest-repos.js
+++ app/scripts/latest-repos.js
@@ -3,12 +3,20 @@
     select: document.querySelector.bind(document)
   };
 
-  const template = dom.select('#latest-repos-template').content;
   const container = dom.select('#latest-repos');
+  const placeholderTemplate = dom.select('#latest-repo-placeholder').content;
+  const template = dom.select('#latest-repos-template').content;
+
+  for (let i = 0; i <= 6; i += 1) {
+    const placeholder = placeholderTemplate.cloneNode(true);
+    container.appendChild(document.importNode(placeholder, true));
+  }
 
   try {
     const response = await $.getJSON({url: 'https://gh-latest-repos-fmyaneprcd.now.sh'});
     const repos = response.reverse().filter(repo => Boolean(repo.description));
+
+    container.innerHTML = '';
 
     for (const repo of repos) {
       const content = template.cloneNode(true);
