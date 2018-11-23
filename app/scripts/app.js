c63f22e0e4bdd9bad778113db8799480abe689f4
Chris Vogt <mail@chrisvogt.me>
2018-11-23 13:34:27 -0800

    refactor: quotes to vanilla ES6 without jQuery (#72)

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -11,7 +11,6 @@ const C1V0 = {
   config: {
     analytics: 'UA-33558417-1',
     projects: 'https://chrisvogt.firebaseio.com/v1/projects.json',
-    quotes: 'https://cdn.rawgit.com/chrisvogt/49b51791348a09cbddb0/raw/585d1712885dda5c13d63c17b5e093d543640e42/book-quotes.json',
     social: 'https://chrisvogt.firebaseio.com/v1/profiles.json',
     hours: 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20json%20where%20url%3D%22https%3A%2F%2Fstats.chrisvogt.me%2Freports%2Fdashboard.json%22&diagnostics=true'
   },
