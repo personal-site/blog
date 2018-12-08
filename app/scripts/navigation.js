6104aabcb66fe34c50dd719c770140262f79c8ef
Chris Vogt <mail@chrisvogt.me>
2018-12-08 11:43:58 -0800

    refactor: remove dependency on jQuery, except AJAX calls

---
+++ app/scripts/navigation.js
@@ -0,0 +1,38 @@
+(() => {
+  /**
+   * Cache of commonly-accessed navigation objects.
+   * @type {Object}
+   */
+  const element = {
+    /** @type {jQuery} Navigation component container. */
+    $primaryNav: $('#primary-nav')
+  };
+
+  /**
+   * Fadable header
+   *
+   * On large screens: hides the header initially
+   * and then fades it after scrolling.
+   */
+  const fadableHeader = () =>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  {
+    const {
+      $primaryNav
+    } = element;
+
+    $primaryNav.removeClass('sticky');
+
+    $(document).on('scroll', () => {
+      if ($(document).width() > 640) {
+        if ($(document).scrollTop() > 420) {
+          $primaryNav.addClass('sticky fixed');
+        } else {
+          $primaryNav.removeClass('sticky fixed');
+          $('body').removeClass('f-topbar-fixed');
+        }
+      }
+    });
+  };
+
+  $.scrollUp();
+  fadableHeader();
+})();
