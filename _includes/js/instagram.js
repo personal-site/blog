05595d4a99c8143019d6785d1088b16f39b3f9d5
Chris Vogt <mail@chrisvogt.me>
2019-01-10 00:35:34 -0800

    refactor: use config settings for Instagram component

--- _includes/js/instagram.js
+++ _includes/js/instagram.js
@@ -1,30 +1,46 @@
-export default () => {
-  /**
-   * Instagram via instafeed.js
-   *
-   * @link https://instafeedjs.com/
-   */
-  const userFeed = new Instafeed({
-    get: 'user',
-    userId: '1546421127',
-    clientId: '7e07fcf783e746b3a236341049fa3cc0',
-    accessToken: '1546421127.1677ed0.362078a0d37a4fcb8bcbf520a4459cdd',
-    sortBy: 'most-recent',
-    limit: 18,
-    template: '<li><a href="{{link}}" class="hvr-shadow-radial" title="View on Instagram"><img src="{{image}}" alt="{{caption}}"></a></li>',
-    success: () => {
-      $('#photos').removeClass('hidden');
-      $('#photos .js-load-more').click(() => {
-        if (feed && typeof feed.next === 'function') {
-          feed.next();
-        }
-      });
-    },
-    error: err => {
-      console.warn('Error loading Instagram photos', err);
-      $('#primary-nav a[href="#photos"]').parent('li').addClass('hidden');
+export default ({config, jQuery}) => {
+  const {
+    instagram: {
+      accessToken,
+      clientId,
+      userId
+    } = {}
+  } = config;
+
+  try {
+    if (!accessToken || !clientId || !userId) {
+      throw new Error('An access_token, client_id, and user_id are required.');
     }
-  });
-  userFeed.run();
-  window.feed = userFeed;
+
+    /**
+     * Instagram via instafeed.js
+     *
+     * @link https://instafeedjs.com/
+     */
+    const userFeed = new Instafeed({
+      get: 'user',
+      accessToken,
+      clientId,
+      userId,
+      sortBy: 'most-recent',
+      limit: 18,
+      template: '<li><a href="{{link}}" class="hvr-shadow-radial" title="View on Instagram"><img src="{{image}}" alt="{{caption}}"></a></li>',
+      success: () => {
+        jQuery('#photos').removeClass('hidden');
+        jQuery('#photos .js-load-more').click(() => {
+          if (feed && typeof feed.next === 'function') {
+            feed.next();
+          }
+        });
+      },
+      error: err => {
+        console.warn('Error loading Instafeed', err);
+        jQuery('#primary-nav a[href="#photos"]').parent('li').addClass('hidden');
+      }
+    });
+    userFeed.run();
+    window.feed = userFeed;
+  } catch (error) {
+    console.warn('Error loading Instagram component', error);
+  }
 };
