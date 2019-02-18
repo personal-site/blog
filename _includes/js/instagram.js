a33dd1e62dc4332444b9bf8ebc8bcd31bb27ce17
Chris Vogt <mail@chrisvogt.me>
2019-02-17 22:48:03 -0800

    feat: support previewing Instagram photos fetched from the Personal API (#108)

--- _includes/js/instagram.js
+++ _includes/js/instagram.js
@@ -1,46 +1,62 @@
-export default ({config, jQuery}) => {
+import renderInstagramModal from './renderers/instagram-modal';
+import transformPhotos from './utils/transform-photos';
+
+export default async ({config, dom, jQuery}) => {
   const {
-    instagram: {
-      accessToken,
-      clientId,
-      userId
-    } = {}
+    instagram: url
   } = config;
 
+  const thumbnailContainer = dom.select('ul#feed');
+  const thumbnailTemplate = dom.select('#instagram-thumbnail-template').content;
+
   try {
-    if (!accessToken || !clientId || !userId) {
-      throw new Error('An access_token, client_id and user_id are required.');
+    if (!url || typeof url !== 'string') {
+      throw new Error('Unable to load Instagram without a config URL.');
     }
 
-    /**
-     * Instagram via instafeed.js
-     *
-     * @link https://instafeedjs.com/
-     */
-    const userFeed = new Instafeed({
-      get: 'user',
-      accessToken,
-      clientId,
-      userId,
-      sortBy: 'most-recent',
-      limit: 18,
-      template: '<li><a href="{{link}}" class="hvr-shadow-radial" title="View on Instagram"><img src="{{image}}" alt="{{caption}}"></a></li>',
-      success: () => {
-        jQuery('#photos').removeClass('hidden');
-        jQuery('#photos .js-load-more').click(() => {
-          if (feed && typeof feed.next === 'function') {
-            feed.next();
-          }
-        });
-      },
-      error: err => {
-        console.warn('Error loading Instafeed', err);
-        jQuery('#primary-nav a[href="#photos"]').parent('li').addClass('hidden');
-      }
-    });
+    try {
+      const {getJSON} = jQuery;
+      const {result: {photos} = {}} = await getJSON({url});
 
-    userFeed.run();
-    window.feed = userFeed;
+      const filtered = photos.filter(photo => photo.type === 'image');
+      const photosObj = filtered.reduce(transformPhotos, {});
+
+      const getClickHandler = id => {
+        const selected = photosObj[id];
+
+        if (!selected) {
+          return;
+        }
+
+        const handleThumbnailClick = event => {
+          renderInstagramModal({dom, photo: selected});
+          event.preventDefault();
+        };
+
+        return handleThumbnailClick;
+      };
+
+      for (const photo of filtered.slice(0, 12)) {
+        const content = thumbnailTemplate.cloneNode(true);
+
+        const img = content.querySelector('.ig-thumb-image');
+        img.src = photo.images.thumbnail.url;
+        img.height = photo.images.thumbnail.height;
+        img.width = photo.images.thumbnail.width;
+
+        const link = content.querySelector('.ig-thumb-link');
+        link.dataset.id = photo.id;
+
+        thumbnailContainer.appendChild(document.importNode(content, true));
+      }
+
+      const links = thumbnailContainer.getElementsByClassName('ig-thumb-link');
+      [...links].forEach(link => link.addEventListener('click', getClickHandler(link.dataset.id)));
+
+      $('#photos').removeClass('hidden');
+    } catch (error) {
+      console.log(error);
+    }
   } catch (error) {
     console.warn('Error loading Instagram component', error);
   }
---
+++ _includes/js/renderers/instagram-modal.js
@@ -0,0 +1,53 @@
+const getRelativeTimeSinceString = date => {
+  const {timeago} = $;
+  const timeInSeconds = Number(date) * 1000;
+  return timeago ? timeago(timeInSeconds) : new Date(date).toISOString();
+};
+
+export default ({dom, photo}) => {
+  if (!dom) {
+    console.log('A dom object is required');
+  }
+
+  if (!photo) {
+    console.log('A photo object is required');
+  }
+
+  const container = dom.select('#photos');
+  const template = dom.select('#instagram-modal-template').content;
+  const content = template.cloneNode(true);
+
+  content.querySelector('.ig-comments-count').textContent = photo.commentsCount;
+  content.querySelector('.ig-likes-count').textContent = photo.likesCount;
+  content.querySelector('.img-modal-avatar').src = photo.profilePicture;
+  content.querySelector('.ig-modal-image').src = photo.images.standard_resolution.url;
+  content.querySelector('.ig-modal-link').href = photo.link;
+  content.querySelector('.ig-modal-location').textContent = photo.locationName;
+  content.querySelector('.ig-modal-text').textContent = photo.text;
+  content.querySelector('.ig-time-ago').textContent = getRelativeTimeSinceString(photo.createdAt);
+
+  const usernameLink = content.querySelector('.ig-modal-username-link');
+  usernameLink.href = `https://instagram.com/${photo.username}`;
+  usernameLink.title = photo.fullName;
+
+  const closeModal = event => {
+    dom.select('#instagram-overlay').remove();
+    dom.select('body').classList.remove('ig-modal-open');
+    event.preventDefault();
+  };
+
+  const handleCloseClick = event => {
+    if (event.key === 'Escape') {
+      closeModal(event);
+      event.target.removeEventListener('keyup', handleCloseClick);
+    }
+  };
+
+  const node = document.importNode(content, true);
+  dom.select('body').classList.add('ig-modal-open');
+
+  node.querySelector('.ig-modal-close').addEventListener('click', closeModal);
+  document.addEventListener('keyup', handleCloseClick);
+
+  container.appendChild(node);
+};
