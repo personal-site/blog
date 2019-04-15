1b4a2655a93750d45cf8dd60cf6cbb4febed7163
Chris Vogt <mail@chrisvogt.me>
2019-04-15 04:18:50 -0700

    PARTIAL -- continue refactoring styles

--- _includes/js/quotes.js
+++ _includes/js/quotes.js
@@ -23,21 +23,30 @@ export default async ({config, dom, jQuery}) => {
       throw new Error('No quotes found.');
     }
 
+    let i = 0;
     for (const quote of quotes) {
       const {cite, body} = quote;
       const content = template.cloneNode(true);
       const blockquote = content.querySelector('.quote-content');
 
+      if (i === 0) {
+        const carouselItem = content.querySelector('.carousel-item');
+        carouselItem.classList.add('active');
+      }
+
       const citeEl = document.createElement('cite');
       citeEl.append(document.createTextNode(cite));
+      citeEl.classList.add('d-block');
 
       blockquote.append(document.createTextNode(body));
       blockquote.append(citeEl);
 
       fragment.append(content);
+      i++;
     }
 
     container.append(document.importNode(fragment, true));
+    $('.carousel').carousel()
   } catch (error) {
     console.warn('Error loading quotes section', error);
   }
